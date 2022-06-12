75e6055c5c5b45bc8549488129581c5b6f17d2d6
marktodman <drmtodman@gmail.com>
2022-06-12 14:14:09 +0000

    Final challenge

--- reduce.js
+++ reduce.js
@@ -6,7 +6,19 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    'Accumulator:', acc,
+    'Current Value:', curr,
+    'Total:', acc + curr
+  );
+  return acc + curr;
+}, 10);
+console.log(sum);
 
+let sum2 = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum2);
 
 const teamMembers = [
   {
@@ -28,10 +40,29 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Mark',
+    profession: 'Manager',
+    yrsExperience: 10
   }
 ];
 
 // Totaling a specific object property
 
+let totalExprience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExprience);
 
 // Grouping by a property, and totaling it too
+
+let exepreienceByProfession = teamMembers.reduce((acc, curr)=> {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {})
+
+console.log(exepreienceByProfession);
